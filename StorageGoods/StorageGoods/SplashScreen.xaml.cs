774da01333ff1edc2928f660af877e95af980e42
Dobzhanskuy Volodymyr <vovadob99044@gmail.com>
2017-09-04 10:25:11 +0300

    SplashScreen
    Add image, but not view

---
+++ StorageGoods/StorageGoods/SplashScreen.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StorageGoods
+{
+    /// <summary>
+    /// Interaction logic for SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashScreen_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void SplashScreen_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var image = sender as Image;
+            var bitmap = Properties.Resources.storage;
+
+            if (image != null)
+            {
+                var result = Imaging.CreateBitmapSourceFromHBitmap(
+                    bitmap.GetHbitmap(),
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+                image.Stretch = Stretch.None;
+                image.Source = result;
+            }
+        }
+    }
+}
